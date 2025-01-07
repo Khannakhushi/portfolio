57e1aab67cd1a6c6e8c6eb13efcf7e6b1ecdc074
Khyaati Khanna <khannakhushi93@gmail.com>
2025-01-06 22:57:45 -0600

    update metadata description in layout component to reflect current academic and career aspirations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,10 @@ const m = Montserrat({ subsets: ['latin'], variable: '--font-montserrat' });
 
 export const metadata: Metadata = {
   title: 'Khyaati Khanna',
-  description: 'I am Khushi :)',
+  description: `Hello! I'm Khyaati Khanna, a passionate software engineer and a junior at the University of Houston,
+                    pursuing a major in Computer Science with minors in Business Administration and
+                    Mathematics. Set to graduate in December 2025, I'm on a journey to become a
+                    versatile full-stack developer.`,
 };
 
 export default function RootLayout({
