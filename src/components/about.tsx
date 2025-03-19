6d880f546d7c11e4c8cc82ca7d063b7f01c7034c
Khyaati Khanna <khannakhushi93@gmail.com>
2025-03-18 21:49:20 -0500

    refactor About component to improve resume link styling and layout

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -37,10 +37,10 @@ export default function About() {
                 <div className="font-playfair text-lg">
                   <h3 className="mb-4 text-2xl font-semibold">Hello, I&apos;m Khyaati Khanna</h3>
                   <p className="mb-4">
-                    I&apos;m a passionate software engineer and a junior at the University of Houston,
-                    pursuing a major in Computer Science with minors in Business Administration and
-                    Mathematics. Set to graduate in December 2025, I&apos;m on a journey to become a
-                    versatile full-stack developer.
+                    I&apos;m a passionate software engineer and a junior at the University of
+                    Houston, pursuing a major in Computer Science with minors in Business
+                    Administration and Mathematics. Set to graduate in December 2025, I&apos;m on a
+                    journey to become a versatile full-stack developer.
                   </p>
                   <div className="mb-4 flex items-center">
                     <GraduationCap className="mr-2" />
@@ -68,16 +68,29 @@ export default function About() {
                       ))}
                     </div>
                   </div>
-                  <Button asChild size="lg">
-                    <Link
-                      href="/Khanna_Resume.pdf"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <StickyNote className="mr-2" />
+                  <Link
+                    href="/Khanna_Resume.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className=" relative ml-0 block max-w-[200px] transform-gpu cursor-pointer overflow-hidden rounded-full border-2 border-transparent p-[0.175rem] text-center text-sm font-semibold leading-6 text-primary shadow-2xl shadow-secondary transition-all duration-500 hover:shadow-md"
+                  >
+                    <motion.div
+                      className="absolute inset-0 h-10 rounded-full bg-secondary-foreground opacity-90 hover:opacity-100"
+                      animate={{
+                        rotate: [0, 360],
+                      }}
+                      transition={{
+                        duration: 8,
+                        repeat: Infinity,
+                        ease: 'linear',
+                        times: [0, 1],
+                      }}
+                    />
+                    <span className="relative z-10 flex items-center justify-center rounded-full bg-secondary px-8 h-10">
+                      <StickyNote className="mr-1 h-4 w-4" />
                       My Resume
-                    </Link>
-                  </Button>
+                    </span>
+                  </Link>
                 </div>
               </CardContent>
             </Card>
